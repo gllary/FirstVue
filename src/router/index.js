1c9d1417641d6bdd58f1d7e47123fc092140f39d
Gyx <1549624673@whut.edu.cn>
2019-11-29 12:07:05 +0800

    添加城市选择功能页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
@@ -8,11 +9,13 @@ export default new Router({
   mode:'history',
   scrollBehavior:() => ({y:0}),
   base:'/ibms/',
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Home',
       component: Home
-    }
-  ]
+    },{
+      path: '/city',
+      name: 'City',
+      component: City
+    }]
 })
