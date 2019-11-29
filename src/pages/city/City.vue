699e2c461376b91761d4dbcaeb1f5131bd0d9db3
Gyx <1549624673@whut.edu.cn>
2019-11-29 15:57:15 +0800

    ajax实现城市列表

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,48 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from './components/Header'
     import CitySearch from './components/Search'
+    import CityList from './components/List'
+    import CityAlphabet from './components/Alphabet'
+    import axios from 'axios'
     export default{
         name:'City',
         components: {
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
 
+        },
+        data () {
+            return {
+                cities: {},
+                hotCities: []
+            }
+        },
+        methods:{
+            getCityInfo () {
+                axios.get('/api/city.json')
+                .then(this.handleGetCityInfo)
+            },
+            handleGetCityInfo (res) {
+                res = res.data
+                if(res.ret && res.data){
+                    const data = res.data
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                }
+            }
+        },
+        mounted () {
+            this.getCityInfo()
         }
     }
 </script>
